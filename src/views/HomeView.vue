e6ede35872779a6ecf953e8fadd7ba08dce35dd5
alifkhan2610 <alifkhan.bubt@gmail.com>
2024-09-19 17:02:42 +0600

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListing from '@/components/JobListings.vue'
+</script>
+
+<template>
+  <Hero />
+  <HomeCards />
+  <JobListing :limit="3" :showButton="true" />
+</template>
